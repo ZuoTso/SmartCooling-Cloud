dbfa5cf23d17d55d1be3d182427f6f47751cfaae
邱聖佐 <chiushengzuo@gmail.com>
2025-02-28 17:18:56 +0800

    Updated Td calculation method to use PsychroLib

--- air_conditioning_env.py
+++ air_conditioning_env.py
@@ -16,10 +16,10 @@ class AirConditioningEnv(gym.Env):
 
         # make sure CSV included T_outside and AH
         if "氣溫(℃)" not in self.weather_data.columns or "AH(g/m³)" not in self.weather_data.columns:
-            raise ValueError("CSV 檔案缺少必要的欄位 '氣溫(℃)' 和 'AH(g/m³)'！")
+            raise ValueError("CSV 檔案缺少必要的欄位 '氣溫(℃)' 或 'AH(g/m³)'！")
 
 
-        # State space: indoor temperature, outdoor temperature, power consumption
+        # State space: ac temperature, outdoor temperature, power consumption, THI, indoor temperature
         self.observation_space = spaces.Box(
             # Minimum ac temperature 16°C, outdoor -1°C, power consumption 0 kWh, THI 0, indoor -1°C
             low=np.array([16, -1, 0, 0, -1], dtype=np.float32),
@@ -32,24 +32,22 @@ class AirConditioningEnv(gym.Env):
         self.action_space = spaces.Discrete(3)
 
         # Initialize environment variables
-        self.T_ac = 25  # Initial ac temperature
-        self.current_index = np.random.randint(0, len(self.weather_data) - self.episode_length) # Random starting point, To track the number of CSV data currently read
+        self.T_ac = 25
+        # Random starting point, To track the number of CSV data currently read
+        self.current_index = np.random.randint(0, len(self.weather_data) - self.episode_length)
         self.T_outside = self.weather_data.iloc[self.current_index]["氣溫(℃)"]
         self.T_in = self.T_outside  # Initial indoor temperature is the same as the outside temperature
-        self.energy_consumption = 0  # Initial energy consumption
+        self.energy_consumption = 0
 
     # Calculate THI
     def calculate_THI(self, T, AH):
-        Td = self.calculate_Td(T, AH)
+        RH = self.absolute_to_relative_humidity(T, AH)
+        Td = psychrolib.GetTDewPointFromRelHum(T, RH)
         THI = T - 0.55 * (1 - np.exp((17.269 * Td) / (Td + 237.3)) / np.exp((17.269 * T) / (T + 237.3))) * (T - 14)
         if np.isnan(THI):
             print("NaN detected in env THI!")
-            print(f"THI_norm: {THI}, T: {T}, AH: {AH}")
+            print(f"T: {T}, AH: {AH}")
         return THI
-    # Calculate the indoor dew point temperature Td
-    def calculate_Td(self, T, AH):
-        RH = self.absolute_to_relative_humidity(T, AH)
-        return T * RH / 100
 
     # Calculate power consumption
     def calculate_power_consumption(self, T_out, T_in, Cooling_capacity=2):
@@ -90,19 +88,18 @@ class AirConditioningEnv(gym.Env):
         return RH
 
     # Calculate Reward
-    def calculate_reward(self, AH, T_out, T_in, a=1, b=0, k=3, THI_optimal=23, Power_min=0, Power_max=3):
+    def calculate_reward(self, AH, T_out, T_in, a=1, b=0, k=10, THI_optimal=23, Power_min=0, Power_max=3):
         """
-        修改後的 calculate_reward：
-          1. 利用 self.calculate_THI(T_in, AH) 計算 THI
-          2. 計算 PowerConsumption 及其正規化值 Power_norm
-          3. 利用 smooth_reward 函數計算 THI_reward，其中：
-            - THI < 10 或 THI > 30 時，reward = 0
-            - THI 在 10～15 或 27～30 時，reward = 0.05
-            - THI 在 15～19 時，reward = 0.1
-            - THI 在 19～22 或 24～27 時，reward = 0.6
-            - THI 在 22～24 時，reward = 1
-            此函數利用多個 sigmoid 函數在臨界點處平滑過渡
-          4. 最終 reward = a * THI_reward - b * Power_norm
+        1. 利用 self.calculate_THI(T_in, AH) 計算 THI
+        2. 計算 PowerConsumption 及其正規化值 Power_norm
+        3. 利用 smooth_reward 函數計算 THI_reward，其中：
+          - THI < 10 or THI > 30 時，reward = -1
+          - THI 在 10～15 或 27～30 時，reward = -0.95
+          - THI 在 15～19 時，reward = -0.9
+          - THI 在 19～22 或 24～27 時，reward = -0.4
+          - THI 在 22～24 時，reward = 0
+          此函數利用多個 sigmoid 函數在臨界點處平滑過渡
+        4. 最終 reward = a * THI_reward - b * Power_norm
         """
         THI = self.calculate_THI(T_in, AH)
         PowerConsumption = self.calculate_power_consumption(T_out, T_in)
@@ -173,7 +170,7 @@ class AirConditioningEnv(gym.Env):
         else:
             self.T_in = self.T_outside
 
-        THI = self.calculate_THI(self.T_ac, AH)
+        THI = self.calculate_THI(self.T_in, AH)
 
         # Return initial status
         state = np.array([self.T_ac, self.T_outside, PowerConsumption, THI, self.T_in], dtype=np.float32)
